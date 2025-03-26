bf708a86dbd13ff03e5726b9252850fd62779343
GabrielMlimaDF <gabrieldevbrasilia@gmail.com>
2025-03-26 20:48:24 -0300

    Acertando Endpoints

--- GestorTF/Services/UserService.cs
+++ GestorTF/Services/UserService.cs
@@ -1,4 +1,5 @@
 ﻿using GestorTF.Models;
+using GestorTF.ServicesSecurity;
 using GestoTF2.Data;
 using GestoTF2.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -10,17 +11,44 @@ namespace GestorTF.Services
     public class UserService : ControllerBase
     {
         private readonly ContextApp _context;
+        private readonly AuthService _authService;
 
-        public UserService(ContextApp context)
+        public UserService(ContextApp context, AuthService authService)
         {
             _context = context;
+            _authService = authService;
         }
 
-        [HttpGet]
-        public async Task<ActionResult> GetUserAsync()
+
+        public async Task<List<User>> GetUserAsync()
         {
-            var userList = await _context.Users.ToArrayAsync();
-            return Ok(userList);
+            return await _context.Users.ToListAsync();
+        }
+        public async Task<(bool Success, string Message)> RegisterUserAsync(UserRegisterDto model)
+        {
+            if (await _context.Users.AnyAsync(u => u.Email == model.Email))
+                return (false, "Email já está em uso.");
+
+            var (hashedPassword, salt) = _authService.HashPassword(model.Password);
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = model.Name,
+                Email = model.Email,
+                Password = hashedPassword,
+                Salt = salt,
+                IsActive = true,
+                CreateAt = DateTime.UtcNow,
+                RoleId = model.RoleId
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            await AddUserRoleAsync(user);
+
+            return (true, "Usuário registrado com sucesso!");
         }
 
         public async Task AddUserRoleAsync(User user)
