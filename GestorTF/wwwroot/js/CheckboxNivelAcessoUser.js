1d4a770592c50fc37514e076580f265f2da5518d
Gabriel Matos <gabrieldevbrasilia@gmail.com>
2025-05-02 09:21:30 -0300

    Finalização frontend Create User

---
+++ GestorTF/wwwroot/js/CheckboxNivelAcessoUser.js
@@ -0,0 +1,10 @@
+﻿document.getElementById('flexCheckUsuario').addEventListener('change', function () {
+    if (this.checked) {
+        document.getElementById('flexCheckDefaultAdmin').checked = false;
+    }
+});
+document.getElementById('flexCheckDefaultAdmin').addEventListener('change', function () {
+    if (this.checked) {
+        document.getElementById('flexCheckUsuario').checked = false;
+    }
+});
---
+++ GestorTF/wwwroot/js/HandlerMenuCreateUser.js
@@ -0,0 +1,38 @@
+﻿//Handler menu create user
+document.addEventListener("DOMContentLoaded", function () {
+    // Definindo as referências das divs
+    const bl1 = document.getElementById("bl1");
+    const bl2 = document.getElementById("bl2");
+    const bl3 = document.getElementById("bl3");
+
+    const menuBl1 = document.getElementById("menu-bl1");
+    const menuBl2 = document.getElementById("menu-bl2");
+    const menuBl3 = document.getElementById("menu-bl3");
+
+    // Função para mostrar a div ativa e esconder a outra
+    function showActiveDiv(activeDiv, activeMenuItem, inactiveDivs, inactiveMenuItems) {
+        // Exibe a div ativa e esconde as inativas
+        activeDiv.style.display = "block";
+        inactiveDivs.forEach(div => div.style.display = "none");
+
+        // Atualiza a classe "active" no item do menu
+        activeMenuItem.classList.add("active");
+        inactiveMenuItems.forEach(item => item.classList.remove("active"));
+    }
+
+    // Inicializa com a div "Informações Pessoais" visível
+    showActiveDiv(bl1, menuBl1, [bl2, bl3], [menuBl2, menuBl3]);
+
+    // Adiciona os eventos de clique nos itens do menu
+    menuBl1.addEventListener("click", function () {
+        showActiveDiv(bl1, menuBl1, [bl2, bl3], [menuBl2, menuBl3]);
+    });
+
+    menuBl2.addEventListener("click", function () {
+        showActiveDiv(bl2, menuBl2, [bl1, bl3], [menuBl1, menuBl3]);
+    });
+
+    menuBl3.addEventListener("click", function () {
+        showActiveDiv(bl3, menuBl3, [bl1, bl2], [menuBl1, menuBl2]);
+    });
+});
